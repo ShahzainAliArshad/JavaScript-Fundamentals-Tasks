c1ad306b8a378a72e294ab327606d91a440390ce
Zain Ali <121271330+ShahzainAliArshad@users.noreply.github.com>
2023-09-19 03:45:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// ===================== The task: 1 =======================
+console.log("=================== Output to the console ========================================================================")
+console.log("=================== Task No: 1 ========================")
+
+console.log("Hello, World");
+
+console.log("=================== Task No: 2 ========================")
+
+let MyName="Zain Ali";
+console.log(`My Name is "${MyName}"
+I am learning Javascript via Code Sekho bootcamp
+I will become the best developer by hardwork
+I am making commitment to do coding with consistency`
+)
+
+console.log("=================== Variables ======================================================================================")
+console.log("=================== Task No: 1 ========================")
+
+let Name="Zain Ali";
+let age=15;
+console.log("My name is "+Name + " and my age is "+age);
+
+
+console.log("=================== Task No: 2 ========================")
+
+let My_Name="My Name is Zain Ali";
+let learning="\nI am learning Javascript via Code Sekho bootcamp ";
+let hardwork="\nI will become the best developer by hardwork ";
+let consistency="\nI am making commitment to do coding with consistency";
+
+console.log(My_Name+learning+hardwork+consistency);
+
+console.log("=================== Task No: 3 ========================")
+
+let num1=15;
+let num2=10;
+let add=num1+num2;
+let minus=num1-num2;
+let mul=num1*num2;
+let div=num1/num2;
+console.log(`num1=15;
+num2=10;
+The Answers are following ===========
+Addition=${add}
+Minus=${minus}
+Multiply=${mul}
+Division=${div}`);
+
+
+console.log("=================== BMI ==============================================================================================")
+
+console.log("=================== Task No: 1 ========================")
+
+let MarkMass=60;
+let Markheight=1.828;
+let JohnMass=12;
+let JohnHeight=1.706;
+
+console.log("=================== Task No: 2 ========================")
+
+let MarkBMI=MarkMass/(Markheight*Markheight);
+let JohnBMI=JohnMass/(JohnHeight*JohnHeight);
+console.log(`BMI of Mark = ${MarkBMI}`);
+console.log(`BMI of John = ${JohnBMI}`);
+
+console.log("=================== Task No: 3 ========================")
+
+let MarkHigherBMI=Boolean(MarkBMI>JohnBMI);
+
+console.log(`Mark BMI is higher than John's = 
+${MarkHigherBMI}`);
+
+
